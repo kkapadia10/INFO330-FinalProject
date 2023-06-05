1d0ea2479317e3cd20f046a66760d79fcb43b1ab
Kaden Kapadia <kaden@nikkad.com>
2023-06-04 17:38:05 -0700

    Adding 2 queries

--- score_vs_genre.sql
+++
@@ -1,7 +0,0 @@
-SELECT ROW_NUMBER() OVER (ORDER BY m.score DESC) 
-AS row_number, m.names, m.score, g.name AS genre 
-FROM imdb_movies m 
-JOIN genre_id gi ON m.id = gi.movie_id 
-JOIN genre g ON gi.genres = g.genre_id 
-ORDER BY m.score DESC
-LIMIT 100;
