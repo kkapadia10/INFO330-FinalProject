e398e22506ee495a9ea3fc47779c32a73f234ab4
Caleb-Lykken <lykken@uw.edu>
2023-06-05 15:04:49 -0700

    changed name from graph to vizulizations

---
+++ Queries/movies_ordered_by_score.sql
@@ -0,0 +1,5 @@
+SELECT imdb_movies.names, imdb_movies.score
+FROM imdb_movies
+JOIN Language ON imdb_movies.Language_id = Language.lang_id
+WHERE Language.lang_id = 30
+ORDER BY imdb_movies.score DESC;
