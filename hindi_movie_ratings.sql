219c8ce9fd674209b732445d72740dd761b93fa0
Arushi Agarwal <121698199+arushiarushi@users.noreply.github.com>
2023-06-06 20:04:07 -0700

    Delete hindi_movie_ratings.sql

--- hindi_movie_ratings.sql
+++
@@ -1,5 +0,0 @@
-SELECT imdb_movies.names, imdb_movies.score
-  FROM imdb_movies
-  JOIN Language ON imdb_movies.Language_id = Language.lang_id
-  WHERE Language.lang_id = 30
-  ORDER BY imdb_movies.score DESC; 
